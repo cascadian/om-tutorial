a5314dfac4860d948070ab7792611a35fafef69c
Paul Currit <pcurrit@gmail.com>
2016-01-28 15:01:42 -0800

    C_App_Database_Exercises exercise-1

--- src/tutorial/om_tutorial/app_database/exercises.cljs
+++ src/tutorial/om_tutorial/app_database/exercises.cljs
@@ -3,6 +3,9 @@
 (def cars-table
   {
    ; TODO (exercise 1): Add a :cars/by-id table
+   :cars/by-id {1 { :id 1 :make "Nissan" :model "Leaf" }
+                2 { :id 2 :make "Dodge" :model "Dart" }
+                3 { :id 3 :make "Ford" :model "Mustang" }}
    })
 
 (def favorites
